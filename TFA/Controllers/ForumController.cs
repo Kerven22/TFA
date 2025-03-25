7b71735e211246a3f72439516aa84220e3c2b494
Kerven Jumayev <kerwen.jumazew@gmail.com>
2025-03-25 16:25:59 +0300

    adding controller

---
+++ TFA/Controllers/ForumController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TFA.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class ForumController:ControllerBase
+    {
+        [HttpGet("forums")]
+        public IActionResult GetForums(CancellationToken cancellationToken)
+        {
+            return Ok(); 
+        }
+    }
+}
---
+++ TFA/Models/Responsies/ForumResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace TFA.Models.Responsies
+{
+    public class ForumResponse
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
